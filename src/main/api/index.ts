339d810fdb9fc81237dcce0b8b7f9f3d33fa017b
loxt <emannuelloxt@hotmail.com>
2023-01-27 23:06:06 -0400

    feat: add build config to tsconfig

--- src/main/api/index.ts
+++ src/main/api/index.ts
@@ -1,6 +1,1 @@
 import "../config/module-alias";
-import { PersonController } from "@/application/controllers";
-
-const person = new PersonController();
-person.speak("TypeScript"); // Olá TYPESCRIPT!
-person.speak(); // Olá mundo!
